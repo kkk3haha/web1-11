a5da16536040bbca9e74143eaf34cce643c9aefd
kkk3haha <lieutenantcommander.mystical@gmail.com>
2017-06-19 17:27:23 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,10 +1,8 @@
 var number = Math.floor(Math.random()*3);
-
+var answer = parseInt(window.prompt('数あてゲーム。0~2の数字を入力してください！'));
 
 var message;
-while(true){
-  var answer = parseInt(window.prompt('数あてゲーム。0~2の数字を入力してください！'));
-  
+ 
 if(answer === number){
   message= 'あたり！';
   break;
@@ -15,5 +13,5 @@ if(answer === number){
 }else{
   message = 'o~3の数字を入力してください';
 }
-}
+
 document.getElementById('choice').textContent = message;
