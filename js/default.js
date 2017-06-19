c6c12ef8b4461a4ca3d6f53d9d54faaa0553c565
kkk3haha <lieutenantcommander.mystical@gmail.com>
2017-06-19 17:40:51 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -14,6 +14,7 @@ if(answer === number){
 }else{
   message = 'o~3の数字を入力してください';
 }
+  document.getElementById('choice').textContent = message;
  }
 
-document.getElementById('choice').textContent = message;
+
